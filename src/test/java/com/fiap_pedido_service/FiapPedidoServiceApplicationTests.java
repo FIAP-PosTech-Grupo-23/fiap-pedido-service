22c0bafb3099710f8f967f27814bd8b802881528
diego-cruz <diegoaugustodacruz@gmail.com>
2025-06-20 10:58:48 -0300

    iniciando projeto pedido

---
+++ src/test/java/com/fiap_pedido_service/FiapPedidoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fiap_pedido_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiapPedidoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
